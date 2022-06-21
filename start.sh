0cda8cbd31547cbf46369095420af292b2bab987
MartynovAA <MartynovAA@akbarsdigital.ru>
2022-06-21 10:49:40 +0300

    Init commit 2

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export BASE_URL=http://localhost
+docker-compose up -d --build
+docker-compose run back migrate
+docker-compose run back loaddata
+docker-compose run back collectstatic
+docker-compose exec back ./manage.py create_super_user --username admin --password admin
